79f932b7ef48b0adc7fc614423440c9c08856d7e
RavikumarKaipu <135000243+RavikumarKaipu@users.noreply.github.com>
2023-06-19 22:50:01 +0530

    Add files via upload

---
+++ Rsuccess.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const RegistrationSuccess = () => {
+  return (
+    <div className="container text-center">
+      <h3>You have signed up successfully!</h3>
+      <h4>
+        <a href="/">Click here to Login</a>
+      </h4>
+    </div>
+  );
+};
+
+export default RegistrationSuccess;
---
+++ Signup.js
@@ -0,0 +1,63 @@
+import React, { useState } from "react";
+import axios from "axios";
+import { useNavigate, Link } from "react-router-dom";
+
+function Signup() {
+  const navigate = useNavigate();
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  async function submit(e) {
+    e.preventDefault();
+
+    try {
+      await axios
+        .post("http://localhost:8000/signup", {
+          email,
+          password,
+        })
+        .then((res) => {
+          if (res.data === "exist") {
+            alert("User already exists");
+          } else if (res.data === "notexist") {
+            // Assuming successful registration redirects to the home page
+            navigate("/home", { state: { id: email } });
+          }
+        })
+        .catch((error) => {
+          alert("Failed to register. Please check your details.");
+          console.log(error);
+        });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  return (
+    <div className="login">
+      <h1>Signup</h1>
+      <form>
+        <input
+          type="email"
+          onChange={(e) => setEmail(e.target.value)}
+          placeholder="Email"
+          required
+        />
+        <input
+          type="password"
+          onChange={(e) => setPassword(e.target.value)}
+          placeholder="Password"
+          required
+        />
+        <button type="submit" onClick={submit}>Submit</button>
+      </form>
+      <br />
+      <p>OR</p>
+      <br />
+      <Link to="/">Login Page</Link>
+    </div>
+  );
+}
+
+export default Signup;
